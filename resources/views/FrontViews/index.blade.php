bd91f1ce0ce4df3fae6335bdb338d042a6a9650d
gus ryan <rian.putra2003@gmail.com>
2024-10-20 23:34:28 +0800

    set the hero for landing page

--- resources/views/FrontViews/index.blade.php
+++ resources/views/FrontViews/index.blade.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>Worshop Website</title>
 </head>
 <body>
-    
+    <div class="w-full">
+        <div class="relative">
+            <img src="/workshop-hero.png" alt="Hero Image" class="w-full h-screen object-cover">
+            <div class="absolute left-0 top-0 w-full h-screen bg-gradient-to-r from-[#EE2E45] to-transparent">
+
+            </div>
+        </div>
+    </div>
 </body>
 </html>
