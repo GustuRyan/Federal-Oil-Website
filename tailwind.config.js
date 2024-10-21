42ba408db1c9dc63a344b687896d9740cc4cb95c
gus ryan <rian.putra2003@gmail.com>
2024-10-21 23:44:00 +0800

    set the main colors to reusable in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
       backgroundImage: {
         'hero-gradient': 'linear-gradient(to right, #EE2E45 75%, transparent 25%)',
       },
+      colors: {
+        'light-red' : '#FFEDEF',
+        'bold-red' : '#EE2E45',
+        'light-blue' : '#F0F6FF',
+        'bold-blue' : '#0E3673',
+        'primary-green' : '#1DC88A',
+      }
     },
   },
   plugins: [],
