db05a4c0cdd715dac37d5e1547db1be1ecb64272
gus ryan <rian.putra2003@gmail.com>
2024-10-31 18:52:00 +0800

    add the livewire for the cashier table

---
+++ resources/views/livewire/chasier-table.blade.php
@@ -0,0 +1,142 @@
+<div class="space-y-5 w-full text-lg">
+    <div class="rounded-xl p-6 flex justify-between bg-light-red">
+        <form class="flex">
+            <input type="text" name="" id="" class="bg-white rounded-l-xl border p-3">
+            <button class="bg-bold-blue text-white rounded-r-xl p-3">
+                Search
+            </button>
+        </form>
+        <button class="rounded-xl bg-primary-green p-3 flex items-center gap-2 text-white font-bold hover:opacity-85">
+            <p>
+                Tambah Baru
+            </p>
+            <p class="text-3xl leading-none">
+                +
+            </p>
+        </button>
+    </div>
+    <div>
+        <table class="w-full border rounded-xl">
+            <thead>
+                <tr align="center">
+                    <th>No</th>
+                    <th>Kode Barang</th>
+                    <th>Nama Barang</th>
+                    <th>Jumlah</th>
+                    <th>Harga</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>BR123</td>
+                    <td>Ban Dunlop</td>
+                    <td>2 Unit</td>
+                    <td>500.000</td>
+                    <td align="center">
+                        <div class="flex gap-2">
+                            <button class="w-20 flex justify-between bg-bold-blue rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Detail
+                            </button>
+                            <button class="w-20 flex justify-between bg-yellow-500 rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Edit
+                            </button>
+                            <button class="w-20 flex justify-between bg-bold-red rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Delete
+                            </button>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>BR123</td>
+                    <td>Ban Dunlop</td>
+                    <td>2 Unit</td>
+                    <td>500.000</td>
+                    <td align="center">
+                        <div class="flex gap-2">
+                            <button class="w-20 flex justify-between bg-bold-blue rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Detail
+                            </button>
+                            <button class="w-20 flex justify-between bg-yellow-500 rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Edit
+                            </button>
+                            <button class="w-20 flex justify-between bg-bold-red rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Delete
+                            </button>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>BR123</td>
+                    <td>Ban Dunlop</td>
+                    <td>2 Unit</td>
+                    <td>500.000</td>
+                    <td align="center">
+                        <div class="flex gap-2">
+                            <button class="w-20 flex justify-between bg-bold-blue rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Detail
+                            </button>
+                            <button class="w-20 flex justify-between bg-yellow-500 rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Edit
+                            </button>
+                            <button class="w-20 flex justify-between bg-bold-red rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Delete
+                            </button>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>BR123</td>
+                    <td>Ban Dunlop</td>
+                    <td>2 Unit</td>
+                    <td>500.000</td>
+                    <td align="center">
+                        <div class="flex gap-2">
+                            <button class="w-20 flex justify-between bg-bold-blue rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Detail
+                            </button>
+                            <button class="w-20 flex justify-between bg-yellow-500 rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Edit
+                            </button>
+                            <button class="w-20 flex justify-between bg-bold-red rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Delete
+                            </button>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>BR123</td>
+                    <td>Ban Dunlop</td>
+                    <td>2 Unit</td>
+                    <td>500.000</td>
+                    <td align="center">
+                        <div class="flex gap-2">
+                            <button class="w-20 flex justify-between bg-bold-blue rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Detail
+                            </button>
+                            <button class="w-20 flex justify-between bg-yellow-500 rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Edit
+                            </button>
+                            <button class="w-20 flex justify-between bg-bold-red rounded-lg text-white font-semibold p-2 hover:opacity-80">
+                                Delete
+                            </button>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>  
+        </table>
+        <div class="w-full p-6 flex justify-end gap-4 text-lg text-bold-blue font-semibold border-[3px] border-t-0">
+            <div class="flex items-center gap-2">
+                <p>Subtotal</p>
+                <div class="border-2 rounded-lg p-3 w-40">
+                    Rp. 250.000
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
