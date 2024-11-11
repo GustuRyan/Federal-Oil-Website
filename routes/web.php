9bdcaaa33611d8cde28fdf8f21caa32eeb1b71b6
gus ryan <rian.putra2003@gmail.com>
2024-11-11 23:35:30 +0800

    set up the route for dashboard page in admin view

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('frontviews.index');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('backviews.pages.index');
+    });
+});
