4197a8c23aef3674f8e2735cd8bea4b24991b05c
gus ryan <rian.putra2003@gmail.com>
2024-11-17 23:51:29 +0800

    set the route for admin panel

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('frontviews.index');
-});
+})->name('cashier');
 
 Route::prefix('admin')->group(function () {
     Route::get('/', function () {
         return view('backviews.pages.index');
-    });
+    })->name('admin.dashboard');
+    Route::get('/stocks', function () {
+        return view('backviews.pages.stocks');
+    })->name('admin.stocks');
+    Route::get('/income', function () {
+        return view('backviews.pages.income');
+    })->name('admin.income');
+    Route::get('/spending', function () {
+        return view('backviews.pages.spending');
+    })->name('admin.spending');
+    Route::get('/receivables', function () {
+        return view('backviews.pages.receivables');
+    })->name('admin.receivables');
 });
