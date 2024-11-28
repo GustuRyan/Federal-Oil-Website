b46c7e83c833a581a50ba07983f0a638323de728
gus ryan <rian.putra2003@gmail.com>
2024-11-28 22:25:25 +0800

    update admin panel dashboard index

--- resources/views/BackViews/pages/index.blade.php
+++ resources/views/BackViews/pages/index.blade.php
@@ -1,43 +1,21 @@
 @extends('backviews.layouts.main')
+@php
+    $bgColors = ['bg-[#FFE2E6]', 'bg-[#E9F1FE]', 'bg-[#FFEDEF]', 'bg-[#F6FAFF]'];
+@endphp
 @section('title-content')
     DASHBOARD
 @endsection
 @section('main-content')
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
-    TEST232 <br><br>
+    <div class="w-full">
+        <h1 class="text-3xl font-bold text-bold-blue">
+            Top Report
+        </h1>
+        <div class="w-[77vw] overflow-x-scroll scrollbar-hide">
+            <div class="w-screen py-8 pr-4 flex gap-6">
+                @foreach ($bgColors as $item)
+                    <livewire:admin.dashboard-card :bgColor="$item" />
+                @endforeach
+            </div>
+        </div>        
+    </div>
 @endsection
