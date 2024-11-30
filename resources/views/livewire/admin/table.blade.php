9e66b4b13c1bd5c17e8d66a8e02fa026d7b9cb55
gus ryan <rian.putra2003@gmail.com>
2024-11-30 23:07:49 +0800

    set up the livewire table for admin panel

---
+++ resources/views/livewire/admin/table.blade.php
@@ -0,0 +1,206 @@
+<div>
+    <table class="w-full border rounded-xl">
+        <thead>
+            <tr align="center" class="text-[#515769]">
+                <th>No</th>
+                <th>Kode Barang</th>
+                <th>Nama Barang</th>
+                <th>Jumlah</th>
+                <th>Harga</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>BR123</td>
+                <td>Ban Dunlop</td>
+                <td>2 Unit</td>
+                <td>500.000</td>
+                <td align="center">
+                    <div class="flex gap-2 justify-center">
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-blue rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_detail.svg" alt="" class="w-5 h-5">
+                            Detail
+                        </a> 
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-red rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_delete.svg" alt="" class="w-5 h-5">
+                            Delete
+                        </a> 
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>BR123</td>
+                <td>Ban Dunlop</td>
+                <td>2 Unit</td>
+                <td>500.000</td>
+                <td align="center">
+                    <div class="flex gap-2 justify-center">
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-blue rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_detail.svg" alt="" class="w-5 h-5">
+                            Detail
+                        </a> 
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-red rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_delete.svg" alt="" class="w-5 h-5">
+                            Delete
+                        </a> 
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>BR123</td>
+                <td>Ban Dunlop</td>
+                <td>2 Unit</td>
+                <td>500.000</td>
+                <td align="center">
+                    <div class="flex gap-2 justify-center">
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-blue rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_detail.svg" alt="" class="w-5 h-5">
+                            Detail
+                        </a> 
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-red rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_delete.svg" alt="" class="w-5 h-5">
+                            Delete
+                        </a> 
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>BR123</td>
+                <td>Ban Dunlop</td>
+                <td>2 Unit</td>
+                <td>500.000</td>
+                <td align="center">
+                    <div class="flex gap-2 justify-center">
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-blue rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_detail.svg" alt="" class="w-5 h-5">
+                            Detail
+                        </a> 
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-red rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_delete.svg" alt="" class="w-5 h-5">
+                            Delete
+                        </a> 
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>BR123</td>
+                <td>Ban Dunlop</td>
+                <td>2 Unit</td>
+                <td>500.000</td>
+                <td align="center">
+                    <div class="flex gap-2 justify-center">
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-blue rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_detail.svg" alt="" class="w-5 h-5">
+                            Detail
+                        </a> 
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-red rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_delete.svg" alt="" class="w-5 h-5">
+                            Delete
+                        </a> 
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>BR123</td>
+                <td>Ban Dunlop</td>
+                <td>2 Unit</td>
+                <td>500.000</td>
+                <td align="center">
+                    <div class="flex gap-2 justify-center">
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-blue rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_detail.svg" alt="" class="w-5 h-5">
+                            Detail
+                        </a> 
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-red rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_delete.svg" alt="" class="w-5 h-5">
+                            Delete
+                        </a> 
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>BR123</td>
+                <td>Ban Dunlop</td>
+                <td>2 Unit</td>
+                <td>500.000</td>
+                <td align="center">
+                    <div class="flex gap-2 justify-center">
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-blue rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_detail.svg" alt="" class="w-5 h-5">
+                            Detail
+                        </a> 
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-red rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_delete.svg" alt="" class="w-5 h-5">
+                            Delete
+                        </a> 
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>BR123</td>
+                <td>Ban Dunlop</td>
+                <td>2 Unit</td>
+                <td>500.000</td>
+                <td align="center">
+                    <div class="flex gap-2 justify-center">
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-blue rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_detail.svg" alt="" class="w-5 h-5">
+                            Detail
+                        </a> 
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-red rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_delete.svg" alt="" class="w-5 h-5">
+                            Delete
+                        </a> 
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>BR123</td>
+                <td>Ban Dunlop</td>
+                <td>2 Unit</td>
+                <td>500.000</td>
+                <td align="center">
+                    <div class="flex gap-2 justify-center">
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-blue rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_detail.svg" alt="" class="w-5 h-5">
+                            Detail
+                        </a> 
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-red rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_delete.svg" alt="" class="w-5 h-5">
+                            Delete
+                        </a> 
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>BR123</td>
+                <td>Ban Dunlop</td>
+                <td>2 Unit</td>
+                <td>500.000</td>
+                <td align="center">
+                    <div class="flex gap-2 justify-center">
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-blue rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_detail.svg" alt="" class="w-5 h-5">
+                            Detail
+                        </a> 
+                        <a href="" class="w-fit h-12 py-1 px-2 bg-bold-red rounded-lg text-white flex justify-between items-center gap-2 hover:opacity-85">
+                            <img src="/icons/icon_delete.svg" alt="" class="w-5 h-5">
+                            Delete
+                        </a> 
+                    </div>
+                </td>
+            </tr>
+        </tbody>  
+    </table>
+</div>
