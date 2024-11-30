afd74b5887f91f3181644d446568f5713f779a92
gus ryan <rian.putra2003@gmail.com>
2024-11-30 23:08:54 +0800

    set up the livewire cashflow card for admin panel

---
+++ resources/views/livewire/admin/cashflow-card.blade.php
@@ -0,0 +1,19 @@
+<div class="w-full p-5 space-y-12 font-semibold rounded-lg shadow-md border bg-white">
+    <div class="w-full flex justify-between items-center">
+        <h1 class="text-xl w-28">
+            Pengeluaran Bulan Ini
+        </h1>
+        <img src="/icons/icon_wallet.svg" alt="" class="w-8 h-8">
+    </div>
+    <div class="space-y-2">
+        <h2 class="text-2xl">
+            Rp.6.500.000
+        </h2>
+        <p class="text-xl">
+            24/31
+            <span class="text-lg">
+                hari
+            </span>
+        </p>
+    </div>
+</div>
