772b86faaf3fc83074304bc9628442971f1cee12
gus ryan <rian.putra2003@gmail.com>
2024-12-02 23:15:33 +0800

    set up the product card for admin panel

---
+++ resources/views/livewire/admin/product-card.blade.php
@@ -0,0 +1,31 @@
+<div class="shadow-md bg-white w-fit rounded-lg">
+    <img src="/example_image.png" alt="" class="w-full content-center rounded-t-lg">
+    <div class="p-4 space-y-3 w-full">
+        <div class="space-y-2 w-full">
+            <div class="w-full flex justify-between">
+                <h2 class="text-xl font-bold">
+                    Nama Barang
+                </h2>
+                <p>x50</p>
+            </div>
+            <div class="w-full flex justify-between">
+                <p>Kategori Barang</p>
+                <p>Rp. 500.000</p>
+            </div>
+        </div>
+        <p>
+            Lorem Ipsum is simply dummy text of the...
+        </p>
+        <div class="flex justify-center gap-3">
+            <a href="" class="w-12 h-12 rounded-full bg-bold-blue flex justify-center items-center">
+                <img src="/icons/icon_detail.svg" alt="" class="w-6 h-6">
+            </a>
+            <a href="" class="w-12 h-12 rounded-full bg-[#F7C443] flex justify-center items-center">
+                <img src="/icons/icon_edit.svg" alt="" class="w-6 h-6">
+            </a>
+            <a href="" class="w-12 h-12 rounded-full bg-bold-red flex justify-center items-center">
+                <img src="/icons/icon_delete.svg" alt="" class="w-6 h-6">
+            </a>
+        </div>
+    </div>
+</div>
