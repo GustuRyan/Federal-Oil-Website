0b43fc3a3c21aae92d866708642da06293d6c93a
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2024-12-30 23:42:30 +0800

    set up update page for stock view

---
+++ resources/views/BackViews/pages/stock/update.blade.php
@@ -0,0 +1,14 @@
+@extends('backviews.layouts.main')
+@section('title-content')
+    <div class="flex items-center gap-4">
+        <a href="{{route('admin.stocks.index')}}">
+            <img src="/icons/icon_arrow_back.svg" class="w-8">
+        </a>
+        <div>
+            PERBARUI STOK
+        </div>
+    </div>    
+@endsection
+@section('main-content')
+    
+@endsection
