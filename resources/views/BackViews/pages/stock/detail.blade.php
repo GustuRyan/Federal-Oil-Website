fbd465bef4438edf4727f6184e1e2ed65efd460d
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2024-12-30 23:42:17 +0800

    set up detail page for stock view

---
+++ resources/views/BackViews/pages/stock/detail.blade.php
@@ -0,0 +1,14 @@
+@extends('backviews.layouts.main')
+@section('title-content')
+    <div class="flex items-center gap-4">
+        <a href="{{route('admin.stocks.index')}}">
+            <img src="/icons/icon_arrow_back.svg" class="w-8">
+        </a>
+        <div>
+            DETAIL STOK
+        </div>
+    </div>    
+@endsection
+@section('main-content')
+    
+@endsection
