06c0906d5c3f0c79e8492666b416b9a1a604b4ab
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2024-12-30 23:42:03 +0800

    set up create page for stock view

---
+++ resources/views/BackViews/pages/stock/create.blade.php
@@ -0,0 +1,14 @@
+@extends('backviews.layouts.main')
+@section('title-content')
+    <div class="flex items-center gap-4">
+        <a href="{{route('admin.stocks.index')}}">
+            <img src="/icons/icon_arrow_back.svg" class="w-8">
+        </a>
+        <div>
+            STOK BARU
+        </div>
+    </div>    
+@endsection
+@section('main-content')
+    
+@endsection
