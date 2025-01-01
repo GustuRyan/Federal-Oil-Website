3684b766bfe3b073edb2b224201ca836e500aaff
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-01-01 23:02:46 +0800

    set up spending migration table

---
+++ database/migrations/2024_12_25_131507_create_spending_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('spending', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->enum('spending_type', allowed:['biaya operasional', 'biaya suku cadang', 'gaji']);
+            $table->text('distributor')->nullable();
+            $table->float('total_cost');
+            $table->enum('payment_methods', allowed: ['tunai', 'kredit', 'transfer']);
+            $table->text('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('spending');
+    }
+};
