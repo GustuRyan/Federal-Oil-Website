b0edb9d6e59f36a5ec5ac2d789e1376ed4e0013d
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-01-11 18:09:35 +0800

    add Customer Model

---
+++ app/Models/Customer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    /**
+     * Nama tabel yang digunakan oleh model ini.
+     *
+     * @var string
+     */
+    protected $table = 'customer';
+
+    /**
+     * Atribut-atribut yang dapat diisi secara massal (mass assignable).
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'phone_number',
+        'email',
+        'motorcycle_type',
+        'description',
+    ];
+
+    /**
+     * Atribut yang disembunyikan saat serialisasi.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+}
