113dbf2a78ccddf829655aceaf6a027c06df3863
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-01-19 23:11:06 +0800

    update customer table

--- database/migrations/2024_12_25_131356_create_customer_table.php
+++ database/migrations/2024_12_25_131356_create_customer_table.php
@@ -4,22 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('customer', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->text('name');
-            $table->text('address');
-            $table->string('phone_number')->unique();
-            $table->string('email')->unique();
-            $table->string('motorcycle_type');
-            $table->text('description')->nullable();
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id(); 
+            $table->timestamps(); 
+            $table->string('name'); 
+            $table->text('address'); 
+            $table->string('phone_number')->unique(); 
+            $table->string('email')->unique(); 
+            $table->string('motorcycle_type'); 
+            $table->text('description')->nullable(); 
         });
     }
 
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('customer');
+        Schema::dropIfExists('customers');
     }
 };
