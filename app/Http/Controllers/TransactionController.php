35027ac161928ddd151057f4b9f1af488bee8c7e
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-01-20 23:31:32 +0800

    update transaction controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    //
+    public function dashboard(){
+        $products = Cart::where('product_id', '!=', null)->get();
+        $services = Cart::where('service_id', '!=', null)->get();
+      
+      return view('frontviews.index', compact('products', 'services'));
+    }
 }
