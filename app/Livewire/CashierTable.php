0aeebfb95c37d6ef39623ba4eea818a23a2a256f
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-01-22 23:58:20 +0800

    update cashier table livewire backend

--- app/Livewire/CashierTable.php
+++ app/Livewire/CashierTable.php
@@ -3,10 +3,27 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Service;
+use App\Models\Product;
 
 class CashierTable extends Component
 {
     public $type;
+    public $items;
+
+    public $isProductFormVisible = false; 
+    public $isServiceFormVisible = false; 
+
+    public function toggleProductForm()
+    {
+        $this->isProductFormVisible = !$this->isProductFormVisible;
+    }
+
+    public function toggleServiceForm()
+    {
+        $this->isServiceFormVisible = !$this->isServiceFormVisible;
+    }
+
     public function render()
     {
         return view('livewire.cashier-table');
