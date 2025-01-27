83c0354ec127d211b6e080d719a39afe748a271b
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-01-27 23:52:54 +0800

    set up auth middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,8 +10,19 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo($request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        // Arahkan ke rute /auth jika pengguna belum login
+        return $request->expectsJson() ? null : route('auth');
     }
+
+    protected function unauthenticated($request, array $guards)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['message' => 'Anda harus login untuk mengakses halaman ini.'], 401);
+        }
+
+        parent::unauthenticated($request, $guards);
+    }
+
 }
