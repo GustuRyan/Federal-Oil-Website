5c9e1adaaf47f114a44b21577ad7ec349a51ef31
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-02-02 22:49:50 +0800

    update receivable table migration

--- database/migrations/2024_12_25_131526_create_receivable_table.php
+++ database/migrations/2024_12_25_131526_create_receivable_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('receivable', function (Blueprint $table) {
+        Schema::create('receivables', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->foreignId('customer_id')->constrained()->cascadeOnDelete();
@@ -24,6 +24,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('receivable');
+        Schema::dropIfExists('receivables');
     }
 };
