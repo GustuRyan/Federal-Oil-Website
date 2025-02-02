7273d24a20cc3f92691ae74d4c4e34e9b19bcb8a
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-02-02 22:49:13 +0800

    update spending table migration

--- database/migrations/2024_12_25_131507_create_spending_table.php
+++ database/migrations/2024_12_25_131507_create_spending_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('spending', function (Blueprint $table) {
+        Schema::create('spends', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->enum('spending_type', allowed:['biaya operasional', 'biaya suku cadang', 'gaji']);
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('spending');
+        Schema::dropIfExists('spends');
     }
 };
