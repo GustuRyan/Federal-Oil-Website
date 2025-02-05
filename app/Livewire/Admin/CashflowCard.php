aedf0caac1cd90cec5fd46cdd43f316549c3a62f
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-02-05 22:52:39 +0800

    add some variables in cashflow card livewire

--- app/Livewire/Admin/CashflowCard.php
+++ app/Livewire/Admin/CashflowCard.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class CashflowCard extends Component
 {
+    public $title;
+    public $value;
+    public $time;
     public function render()
     {
         return view('livewire.admin.cashflow-card');
