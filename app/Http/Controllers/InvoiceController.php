f1c9a9d6711f2a0fcc7c7d6f3f25f041a037d159
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-02-07 22:25:00 +0800

    update invoice controller

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Spending;
+use App\Models\Transaction;
+use App\Models\TransactionDetail;
 use Illuminate\Http\Request;
 use Barryvdh\Snappy\Facades\SnappyPdf as PDF;
 use Illuminate\Support\Facades\View;
@@ -9,16 +13,57 @@ use App\Charts\MonthlyChart;
 use App\Charts\RevenueChart;
 use App\Charts\MonthlySpendChart;
 use App\Charts\SpendingChart;
+use Carbon\Carbon;
 
 class InvoiceController extends Controller
 {
     public function index(MonthlyChart $chart, RevenueChart $revenueChart, MonthlySpendChart $chart1, SpendingChart $spendChart)
     {
-        $titles = [
-            'Total Item Keluar', 'Total Pendapatan Bulan Ini', 'Total Pengeluaran Bulan Ini'
-        ];
 
-        
+        $currentDate = Carbon::now()->day;
+        $totalDaysInMonth = Carbon::now()->daysInMonth;
+
+        $formattedDate = $currentDate . '/' . $totalDaysInMonth;
+
+        $totalAmount = TransactionDetail::whereNull('service_id')
+            ->whereNotNull('product_id')
+            ->whereMonth('created_at', Carbon::now()->month)
+            ->whereYear('created_at', Carbon::now()->year)
+            ->sum('amount');
+
+        $totalStock = Product::sum('latest_stock');
+
+        $amountPercentage = $totalStock > 0 ? number_format(($totalAmount / $totalStock * 100), 2) : 0;
+        $amountPercentage = $amountPercentage . '%';
+
+        $totalIncome = Transaction::whereMonth('created_at', Carbon::now()->month)
+            ->whereYear('created_at', Carbon::now()->year)
+            ->sum('total_cost');
+
+        $totalSpend = Spending::whereMonth('created_at', Carbon::now()->month)
+            ->whereYear('created_at', Carbon::now()->year)
+            ->sum('total_cost');
+
+        $cardResources = collect([
+            [
+                'color' => 'bg-[#FFE2E6]',
+                'title' => 'Total Item Keluar',
+                'value' => $totalAmount,
+                'time' => $amountPercentage
+            ],
+            [
+                'color' => 'bg-[#E9F1FE]',
+                'title' => 'Total Pendapatan Bulan Ini',
+                'value' => $totalIncome,
+                'time' => $formattedDate
+            ],
+            [
+                'color' => 'bg-[#FFEDEF]',
+                'title' => 'Total Pengeluaran Bulan Ini',
+                'value' => $totalSpend,
+                'time' => $formattedDate
+            ],
+        ]);
 
         // Generate chart data
         $chartData = $chart->build();
@@ -26,40 +71,60 @@ class InvoiceController extends Controller
         $spendData = $chart1->build();
         $chartSpend = $spendChart->build();
 
-        return view('backviews.pages.index', compact('chartData', 'chartRevenue', 'spendData', 'chartSpend'));
+        return view('backviews.pages.index', compact('chartData', 'chartRevenue', 'spendData', 'chartSpend', 'cardResources'));
     }
 
-    public function generatePDF()
+    public function generatePDF($id)
     {
+        $transaction = Transaction::findOrFail($id);
+        $products = TransactionDetail::where('transaction_id', $id)
+            ->whereNotNull('product_id')
+            ->get();
+
+        $services = TransactionDetail::where('transaction_id', $id)
+            ->whereNotNull('service_id')
+            ->get();
+
         $data = [
             'company' => 'PT Kledo Berhati Nyaman',
-            'invoice_no' => 'INV-73333',
+            'invoice_no' => $transaction->invoice,
             'date' => '21/07/2020',
             'due_date' => '23/07/2020',
             'customer' => [
-                'name' => 'Slamet Maryadi Usada',
-                'address' => 'Ps. Peta No. 506, Mojokerto 49077, KalBar',
-                'phone' => '627502388871',
-                'email' => 'uchita.oktaviani@salahudin.desa.id',
+                'name' => $transaction->customer->name,
+                'address' => $transaction->customer->address,
+                'phone' => $transaction->customer->phone_number,
+                'email' => $transaction->customer->email,
             ],
-            'products' => [
-                [
-                    'name' => 'Moslem Brown Blue Dress',
-                    'description' => 'Ukuran M',
-                    'quantity' => 3,
-                    'price' => 199000,
+            'products' => $products->map(function ($product) {
+                return [
+                    'name' => $product->product->product_name ?? 'N/A', 
+                    'description' => $product->product->description ?? '-',
+                    'quantity' => $product->amount,
+                    'price' => $product->product->selling_price,
                     'discount' => 0,
-                    'tax' => 10,
-                    'total' => 597000,
-                ]
-            ],
-            'subtotal' => 542727.27,
-            'tax' => 54722.73,
-            'total' => 597000,
+                    'tax' => 11, 
+                    'total' => $product->product->selling_price*$product->amount*1.11,
+                ];
+            })->toArray(),
+            'services' => $services->map(function ($service) {
+                return [
+                    'name' => $service->service->service_name ?? 'N/A', 
+                    'description' => $service->service->description ?? '-',
+                    'quantity' => $service->service_time.' menit',
+                    'price' => $service->service->service_price,
+                    'discount' => 0,
+                    'tax' => 11, 
+                    'total' => $service->service->service_price*1.11,
+                ];
+            })->toArray(),
+            'subtotal' => round($transaction->total_cost / 1.11, 2),
+            'tax' => round($transaction->total_cost / 1.11 * 0.11, 2),
+            'total' => $transaction->total_cost,
             'bank' => 'XXXXXXXX BCA a/n PT. ABC',
         ];
 
-        $pdf = PDF::loadView('invoice', $data);
+        $pdf = PDF::loadView('invoice', $data)->setPaper('a4', 'landscape');
         return $pdf->download('invoice.pdf');
     }
 }
