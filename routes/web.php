ca417807a4c045d1a9ed97d9c4da161989bafa5d
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-02-09 23:22:27 +0800

    update web.php to add some routes

--- routes/web.php
+++ routes/web.php
@@ -3,115 +3,112 @@
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\QueueController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\ReceivableController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\SpendingController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InvoiceController;
 
-Route::get('/', [TransactionController::class, 'dashboard'])->name('cashier');
+Route::get('/auth', function () {
+    return view('frontviews.auth');
+})->name('auth');
 
-Route::prefix('admin')->group(function () {
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-    Route::get('/', function () {
-        return view('backviews.pages.index');
-    })->name('admin.dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/', [TransactionController::class, 'dashboard'])->name('cashier');
 
-    Route::prefix('stock')->group(function () {
-        Route::get('/', [ProductController::class, 'index'])->name('admin.stocks.index');
-        Route::get('/detail/{id}', [ProductController::class, 'detail'])->name('admin.stocks.detail');
-        Route::get('/create', function () {
-            return view('backviews.pages.stock.create');
-        })->name('admin.stocks.create');
-        Route::get('/update/{id}', [ProductController::class, 'edit'])->name('admin.stocks.update');
-        Route::get('/delete', function () {
-            return view('backviews.pages.stock.index');
-        })->name('admin.stocks.delete');
+    Route::prefix('admin')->group(function () {
+        Route::get('/', [InvoiceController::class, 'index'])->name('admin.dashboard');
+
+        Route::prefix('stock')->group(function () {
+            Route::get('/', [ProductController::class, 'index'])->name('admin.stocks.index');
+            Route::get('/detail/{id}', [ProductController::class, 'detail'])->name('admin.stocks.detail');
+            Route::get('/create', function () {
+                return view('backviews.pages.stock.create');
+            })->name('admin.stocks.create');
+            Route::get('/update/{id}', [ProductController::class, 'edit'])->name('admin.stocks.update');
+            Route::get('/delete', function () {
+                return view('backviews.pages.stock.index');
+            })->name('admin.stocks.delete');
+        });
+
+        Route::prefix('income')->group(function () {
+            Route::get('/', [TransactionController::class, 'index'])->name('admin.income.index');
+            Route::get('/detail/{id}', [TransactionController::class, 'detail'])->name('admin.income.detail');
+            Route::get('/edit/{id}', [TransactionController::class, 'edit'])->name('admin.income.update');
+        });
+
+        Route::prefix('spending')->group(function () {
+            Route::get('/', [SpendingController::class, 'index'])->name('admin.spending.index');
+            Route::get('/create', function () {
+                return view('backviews.pages.spending.create');
+            })->name('admin.spending.create');
+            Route::get('/edit/{id}', [SpendingController::class, 'edit'])->name('admin.spending.update');
+        });
+
+        Route::prefix('receivable')->group(function () {
+            Route::get('/', [ReceivableController::class, 'index'])->name('admin.receivables.index');
+            Route::get('/create', [ReceivableController::class, 'create'])->name('admin.receivables.create');
+            Route::get('/edit/{id}', [ReceivableController::class, 'edit'])->name('admin.receivables.update');
+        });
+
+        Route::prefix('customer')->group(function () {
+            Route::get('/', [CustomerController::class, 'index'])->name('admin.customer.index');
+            Route::get('/create', function () {
+                return view('backviews.pages.customer.create');
+            })->name('admin.customer.create');
+            Route::get('/update/{id}', [CustomerController::class, 'edit'])->name('admin.customer.update');
+        });
+
+        Route::prefix('service')->group(function () {
+            Route::get('/', [ServiceController::class, 'index'])->name('admin.service.index');
+            Route::get('/create', function () {
+                return view('backviews.pages.service.create');
+            })->name('admin.service.create');
+            Route::get('/update/{id}', [ServiceController::class, 'edit'])->name('admin.service.update');
+        });
     });
 
-    Route::prefix('income')->group(function () {
-        Route::get('/', function () {
-            return view('backviews.pages.income.index');
-        })->name('admin.income.index');
-        Route::get('/detail', function () {
-            return view('backviews.pages.stock.index');
-        })->name('admin.income.detail');
-        Route::get('/edit', function () {
-            return view('backviews.pages.stock.index');
-        })->name('admin.income.edit');
-        Route::get('/delete', function () {
-            return view('backviews.pages.stock.index');
-        })->name('admin.income.delete');
-    });
+    Route::get('/queue/get', [QueueController::class, 'index'])->name('get.queue');
+    Route::post('/queues/store', [QueueController::class, 'store']);
+    Route::put('/queues/update/{id}', [QueueController::class, 'update'])->name('update.queue');
+    Route::put('/queues/current/{id}', [QueueController::class, 'updateCurrent']);
+    Route::delete('/queues/{id}', [QueueController::class, 'destroy'])->name('delete.queue');
 
-    Route::prefix('spending')->group(function () {
-        Route::get('/', function () {
-            return view('backviews.pages.spending.index');
-        })->name('admin.spending.index');
-        Route::get('/detail', function () {
-            return view('backviews.pages.stock.index');
-        })->name('admin.spending.detail');
-        Route::get('/edit', function () {
-            return view('backviews.pages.stock.index');
-        })->name('admin.spending.edit');
-        Route::get('/delete', function () {
-            return view('backviews.pages.stock.index');
-        })->name('admin.spending.delete');
-    });
+    Route::post('/customers', [CustomerController::class, 'store'])->name('create.customer');
+    Route::post('/customers', [CustomerController::class, 'storeDashboard'])->name('create.dashboard.customer');
+    Route::put('/customers/{id}', [CustomerController::class, 'update'])->name('update.customer');
+    Route::delete('/customers/{id}', [CustomerController::class, 'destroy'])->name('delete.customer');
 
-    Route::prefix('receivable')->group(function () {
-        Route::get('/', function () {
-            return view('backviews.pages.receivable.index');
-        })->name('admin.receivables.index');
-        Route::get('/detail', function () {
-            return view('backviews.pages.stock.index');
-        })->name('admin.receivable.detail');
-        Route::get('/create', function () {
-            return view('backviews.pages.stock.index');
-        })->name('admin.receivable.create');
-        Route::get('/edit', function () {
-            return view('backviews.pages.stock.index');
-        })->name('admin.receivable.edit');
-        Route::get('/delete', function () {
-            return view('backviews.pages.stock.index');
-        })->name('admin.receivable.delete');
-    });
+    Route::post('/product', [ProductController::class, 'store'])->name('create.product');
+    Route::put('/product/{id}', [ProductController::class, 'update'])->name('update.product');
+    Route::delete('/product/{id}', [ProductController::class, 'delete'])->name('delete.product');
 
-    Route::prefix('customer')->group(function () {
-        Route::get('/', [CustomerController::class, 'index'])->name('admin.customer.index');
-        Route::get('/create', function () {
-            return view('backviews.pages.customer.create');
-        })->name('admin.customer.create');
-        Route::get('/update/{id}', [CustomerController::class, 'edit'])->name('admin.customer.update');
-    });
+    Route::post('/carts', [CartController::class, 'store'])->name('create.cart');
+    Route::put('/carts/{id}', [CartController::class, 'update'])->name('update.cart');
+    Route::delete('/carts/{id}', [CartController::class, 'destroy'])->name('delete.cart');
 
-    Route::prefix('service')->group(function () {
-        Route::get('/', [ServiceController::class, 'index'])->name('admin.service.index');
-        Route::get('/create', function () {
-            return view('backviews.pages.service.create');
-        })->name('admin.service.create');
-        Route::get('/update/{id}', [ServiceController::class, 'show'])->name('admin.service.update');
-    });
+    Route::post('/services', [ServiceController::class, 'store'])->name('store.service');
+    Route::put('/services/{id}', [ServiceController::class, 'update'])->name('update.service');
+    Route::delete('/services/{id}', [ServiceController::class, 'destroy'])->name('delete.service');
+
+    Route::post('/transaction', [TransactionController::class, 'store'])->name('create.transaction');
+    Route::put('/transaction/{id}', [TransactionController::class, 'update'])->name('update.transaction');
+    Route::delete('/transaction/{id}', [TransactionController::class, 'destroy'])->name('delete.transaction');
+
+    Route::post('/receivable', [ReceivableController::class, 'store'])->name('create.receivable');
+    Route::put('/receivable/{id}', [ReceivableController::class, 'update'])->name('update.receivable');
+    Route::delete('/receivable/{id}', [ReceivableController::class, 'destroy'])->name('delete.receivable');
+
+    Route::post('/spending', [SpendingController::class, 'store'])->name('create.spending');
+    Route::put('/spending/{id}', [SpendingController::class, 'update'])->name('update.spending');
+    Route::delete('/spending/{id}', [SpendingController::class, 'destroy'])->name('delete.spending');
+
+    Route::get('/invoice/pdf/{id}', [InvoiceController::class, 'generatePDF'])->name('invoice.pdf');
 });
-
-Route::get('/queue/get', [QueueController::class, 'index'])->name('get.queue');
-Route::post('/queues/store', [QueueController::class, 'store']);
-Route::put('/queues/update/{id}', [QueueController::class, 'update'])->name('update.queue'); 
-Route::put('/queues/current/{id}', [QueueController::class, 'updateCurrent']); 
-Route::delete('/queues/{id}', [QueueController::class, 'destroy'])->name('delete.queue');
-
-Route::post('/customers', [CustomerController::class, 'store'])->name('create.customer');
-Route::post('/customers', [CustomerController::class, 'storeDashboard'])->name('create.dashboard.customer');
-Route::put('/customers/{id}', [CustomerController::class, 'update'])->name('update.customer'); 
-Route::delete('/customers/{id}', [CustomerController::class, 'destroy'])->name('delete.customer');
-
-Route::post('/product', [ProductController::class, 'store'])->name('create.product');
-Route::put('/product/{id}', [ProductController::class, 'update'])->name('update.product');
-Route::delete('/product/{id}', [ProductController::class, 'delete'])->name('delete.product');
-
-Route::post('/carts', [CartController::class, 'store'])->name('create.cart');
-Route::put('/carts/{id}', [CartController::class, 'update'])->name('update.cart');
-Route::delete('/carts/{id}', [CartController::class, 'destroy'])->name('delete.cart');
-
-Route::post('/services', [ServiceController::class, 'store'])->name('store.service');
-Route::put('/services/{id}', [ServiceController::class, 'update'])->name('update.service');
-Route::delete('/services/{id}', [ServiceController::class, 'destroy'])->name('delete.service');
