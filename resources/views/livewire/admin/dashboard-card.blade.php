52ecf5c179ea52fd475c059521868bdf48cbae46
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-02-10 23:31:42 +0800

    update livewire dashboard card

--- resources/views/livewire/admin/dashboard-card.blade.php
+++ resources/views/livewire/admin/dashboard-card.blade.php
@@ -1,24 +1,24 @@
-<div class="w-[400px] h-fit p-6 rounded-3xl shadow-lg space-y-28 {{$bgColor}}">
+<div class="w-[480px] h-fit p-6 rounded-3xl shadow-md space-y-28 {{$bgColor}}">
     <div class="w-full space-y-1">
         <div class="w-full flex justify-between">
             <p class="text-2xl font-bold text-bold-blue">
-                Total Item Keluar
+                {{ $title }}
             </p>
             <div class="w-10 h-10 rounded-full bg-bold-blue"></div>
         </div>
         <p class="text-3xl font-bold text-bold-blue">
-            95
+            {{ number_format($value, 0, ',', '.') }}
         </p>
     </div>
     <div class="w-full flex justify-between">
         <div class="flex gap-2">
             <div class="w-7 h-7 rounded-full bg-bold-blue"></div>
             <p class="text-xl font-bold text-bold-blue">
-                1%
+                {{ $time }}
             </p>
         </div>
         <p class="text-xl font-bold text-bold-blue">
-            2024
+            {{ $year }}
         </p>
     </div>
 </div>
