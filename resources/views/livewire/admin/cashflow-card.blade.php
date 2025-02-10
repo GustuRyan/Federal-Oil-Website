82bc159e12f3cd5f7ec6288cb42ffaaa15daf770
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-02-10 23:32:02 +0800

    update livewire cashflow card

--- resources/views/livewire/admin/cashflow-card.blade.php
+++ resources/views/livewire/admin/cashflow-card.blade.php
@@ -1,19 +1,16 @@
-<div class="w-full p-5 space-y-12 font-semibold rounded-lg shadow-md border bg-white">
+<div class="w-full p-4 space-y-12 font-semibold rounded-lg shadow-md border bg-white">
     <div class="w-full flex justify-between items-center">
-        <h1 class="text-xl w-28">
-            Pengeluaran Bulan Ini
+        <h1 class="text-xl max-w-52">
+            {{ $title }}
         </h1>
-        <img src="/icons/icon_wallet.svg" alt="" class="w-8 h-8">
+        <img src="/icons/icon_wallet.svg" alt="" class="hidden xl:block w-8 h-8">
     </div>
     <div class="space-y-2">
         <h2 class="text-2xl">
-            Rp.6.500.000
+            {{ number_format($value, 0, ',', '.') }}
         </h2>
         <p class="text-xl">
-            24/31
-            <span class="text-lg">
-                hari
-            </span>
+            {{ $time }}
         </p>
     </div>
 </div>
