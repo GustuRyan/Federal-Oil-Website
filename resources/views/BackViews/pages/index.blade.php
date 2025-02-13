11b6ee82be33215a8f7ca7453bf4157376c726bd
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-02-13 22:56:12 +0800

    update dashboard in admin panel

--- resources/views/BackViews/pages/index.blade.php
+++ resources/views/BackViews/pages/index.blade.php
@@ -5,17 +5,41 @@
 @section('title-content')
     DASHBOARD
 @endsection
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    {!! $chartData->script() !!}
+    {!! $chartRevenue->script() !!}
+    {!! $spendData->script() !!}
+    {!! $chartSpend->script() !!}
+@endsection
 @section('main-content')
     <div class="w-full">
         <h1 class="text-3xl font-bold text-bold-blue">
             Top Report
         </h1>
-        <div class="w-[77vw] overflow-x-scroll scrollbar-hide">
+        <div class="w-full lg:w-[77vw] overflow-x-scroll scrollbar-hide space-y-8">
             <div class="w-screen py-8 pr-4 flex gap-6">
-                @foreach ($bgColors as $item)
-                    <livewire:admin.dashboard-card :bgColor="$item" />
+                @foreach ($cardResources as $item)
+                    <livewire:admin.dashboard-card :bgColor="$item['color']" :title="$item['title']" :value="$item['value']" :time="$item['time']"/>
                 @endforeach
             </div>
-        </div>        
+        </div>
+        <div class="w-full flex flex-col md:flex-row justify-between gap-4 md:gap-0 lg:gap-4 mb-4 md:mb-8">
+            <div class="md:w-[49%] lg:w-full p-4 space-y-12 font-semibold rounded-lg shadow-md border bg-white">
+                {!! $chartData->container() !!}
+            </div>
+            <div class="md:w-[49%] lg:w-full p-4 space-y-12 font-semibold rounded-lg shadow-md border bg-white">
+                {!! $chartRevenue->container() !!}
+            </div>
+        </div>
+        <div class="w-full flex flex-col md:flex-row justify-between gap-4 md:gap-0 lg:gap-4">
+            <div class="md:w-[49%] lg:w-full p-4 space-y-12 font-semibold rounded-lg shadow-md border bg-white">
+                {!! $spendData->container() !!}
+            </div>
+            <div class="md:w-[49%] lg:w-full p-4 space-y-12 font-semibold rounded-lg shadow-md border bg-white">
+                {!! $chartSpend->container() !!}
+            </div>
+        </div>
     </div>
 @endsection
