aaa01d264ef63cf7b56bfa3903e464e306191bb8
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-02-19 23:18:03 +0800

    update user queue table migration

--- database/migrations/2025_02_15_165600_create_user_queue_table.php
+++ database/migrations/2025_02_15_165600_create_user_queue_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,7 +14,10 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->integer('queue');
-            $table->foreignId('customer_id')->constrained()->cascadeOnDelete();
+            
+            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->foreign('customer_id')->references('id')->on('customers')->cascadeOnDelete();
+
             $table->text('issue')->nullable();
         });
     }
